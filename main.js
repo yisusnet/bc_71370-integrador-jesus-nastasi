9bbd1c055737dfde7a1cdd31335f9f60d37eb926
jesus nastasi <jesusnastasi@gmail.com>
2024-05-25 00:55:58 -0400

    eliminimo script de scroll mouse del html agrego script al main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import "./sass/main.scss";
+
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+  let st = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (st > lastScrollTop) {
+   
+    document.querySelector('.section-cards__header').classList.add('hidden');
+  } else {
+
+    document.querySelector('.section-cards__header').classList.remove('hidden');
+  }
+  lastScrollTop = st <= 0 ? 0 : st; 
+});
+
