07ebfbd4424206073ea6210cb94b586802361a34
jesus nastasi <jesusnastasi@gmail.com>
2024-05-25 16:49:51 -0400

    agrego script a sectionCardsHeader +nosotrosHeader +contactHeader+ searchBar

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-import "./sass/main.scss";
-
+import './sass/main.scss';
 
 let lastScrollTop = 0;
 
-window.addEventListener("scroll", function() {
+window.addEventListener('scroll', function () {
   let st = window.pageYOffset || document.documentElement.scrollTop;
+  let sectionCardsHeader = document.querySelector('.section-cards__header');
+  let nosotrosHeader = document.querySelector('.nosotros__header');
+  let contactHeader = document.querySelector('.contact__header');
+  let searchBar = document.querySelector('.search-bar');
 
   if (st > lastScrollTop) {
-   
-    document.querySelector('.section-cards__header').classList.add('hidden');
+    if (sectionCardsHeader) sectionCardsHeader.classList.add('hidden');
+    if (nosotrosHeader) nosotrosHeader.classList.add('hidden');
+    if (contactHeader) contactHeader.classList.add('hidden');
+    if (searchBar) searchBar.classList.add('hidden');
   } else {
-
-    document.querySelector('.section-cards__header').classList.remove('hidden');
+    if (sectionCardsHeader) sectionCardsHeader.classList.remove('hidden');
+    if (nosotrosHeader) nosotrosHeader.classList.remove('hidden');
+    if (contactHeader) contactHeader.classList.remove('hidden');
+    if (searchBar) searchBar.classList.remove('hidden');
   }
-  lastScrollTop = st <= 0 ? 0 : st; 
+  lastScrollTop = st <= 0 ? 0 : st;
 });
-
