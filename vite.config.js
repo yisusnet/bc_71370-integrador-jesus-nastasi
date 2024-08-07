ba19338a67cbde9865674444c8ad10b62faf602c
jesus nastasi <jesusnastasi@gmail.com>
2024-08-07 01:59:12 -0400

    modifico el vite.config.js para lograr hacer un npm build correcto y me tome la carpeta template como un handlebars y no como un js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,37 @@
-import { resolve } from "node:path"
-export default {
-    server: {
-        port:1234,
-    },
-    css: {
-        devSourceMap:true,
-    },
-    build: {
-        emptyOutDir: true,
-        rollupOptions:{
-            input: {
-                alta: resolve("views/alta.html"),
-                carrito: resolve("views/carrito.html"),
-                contacto: resolve("views/contacto.html"),
-                mosotros: resolve("views/nosotros.html"),
-                inicio: resolve("index.html"),
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy"; 
+import handlebars from "vite-plugin-handlebars";
 
-            }
+export default defineConfig({
+  server: {
+    port: 1234,
+  },
+  css: {
+    devSourceMap: true,
+  },
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        alta: resolve(__dirname, "views/alta.html"),
+        carrito: resolve(__dirname, "views/carrito.html"),
+        contacto: resolve(__dirname, "views/contacto.html"),
+        nosotros: resolve(__dirname, "views/nosotros.html"),
+        inicio: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  plugins: [
+    handlebars(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "templates/*.hbs",
+          dest: "templates"
         }
-    }
-}
-console.log(resolve())
+      ]
+    })
+  ],
+  assetsInclude: ['**/*.hbs'],
+});
